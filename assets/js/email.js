2bd544ec8645b1ff2063e147e01b30f83e6814e8
madatoo <magda.ruszaj@gmail.com>
2021-04-02 18:27:00 +0000

    validate js and html code and working with email form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,14 +1,15 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail", "sligo_trips", {
-    "from_name": contactForm.name.value,
-    "from_email": contactForm.emailaddress.value,
-    "trip_request": contactForm.trip_request.value
-})
-    .then(function(response) {
-       console.log('Thank you for your request!', response.status, response.text);
-    }, function(error) {
-       console.log('Sorry,try one more time', error);
+//this code is from https://www.emailjs.com/docs/tutorial/creating-contact-form/
+window.onload = function() {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+
+        event.preventDefault();
+
+        emailjs.sendForm("gmail", 'sligo_trips', this)
+            .then(function() {
+                console.log('Thank you for your request!', response.status, response.text);
+            }, function(error) {
+                console.log('Sorry, something went wrong. Try one more time.', error);
+            });
     });
-    return false;
 
 }
