740f1088e1161dd5420346b3404538b82c558380
madatoo <magda.ruszaj@gmail.com>
2021-04-06 13:40:43 +0000

    fix function sendMessage in email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,29 @@
 //this code is from https://www.emailjs.com/docs/tutorial/creating-contact-form/
-window.onload = function() {
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
 
-        event.preventDefault();
+    window.onload = function() {
+        document.getElementById('send_message').addEventListener('submit', function(event) {
+                event.preventDefault();
 
-        emailjs.sendForm("gmail", 'sligo_trips', this)
-            .then(function() {
-                console.log('Thank you for your request!', response.status, response.text);
-            }, function(error) {
+                
+        function sendMail(send_message){
+            
+            emailjs.send("gmail", "sligo_trips", {
+                "from_name":send_message.name.value,
+                "emailaddress": send_message.emailaddress.value,
+                "trip_request": send_message.trip_request.value
+            })
+                
+            .then(
+                
+                function(response) {
+                console.log('Thank you for your request!', response);
+                }, 
+            
+                function(error) {
                 console.log('Sorry, something went wrong. Try one more time.', error);
-            });
-    });
+                }
+            );
 
-}
+            return false;
+            }
+        })}
